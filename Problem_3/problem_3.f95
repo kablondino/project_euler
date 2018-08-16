9bbf21d4328d929c717e71b6c6a0b401f90c2689
kablondino <kablondino@gmail.com>
2018-08-16 15:11:09 +0200

    Started, check how to loop

---
+++ Problem_3/problem_3.f95
@@ -0,0 +1,22 @@
+!
+! Problem 3
+! The prime factors of 13195 are 5, 7, 13 and 29.
+!
+! What is the largest prime factor of the number 600851475143 ?
+
+program problem3
+    implicit none
+    integer*8 :: the_target = 600851475143
+
+    integer*8 :: current
+
+    integer :: i = 3
+    integer :: high = 0
+
+    print *, the_target
+
+!    do,
+!        do ( mod(current, i) .neq. 0 )
+
+end
+
