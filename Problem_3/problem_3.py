f905d9b851d8d1570787532890a62e3e6cc20fd4
kablondino <kablondino@gmail.com>
2018-08-16 14:44:47 +0200

    Working

---
+++ Problem_3/problem_3.py
@@ -0,0 +1,25 @@
+"""
+	Problem 3
+	The prime factors of 13195 are 5, 7, 13 and 29.
+
+	What is the largest prime factor of the number 600851475143 ?
+"""
+
+
+the_target = 600851475143
+current = the_target
+
+i = 3
+
+while True:
+	while( not (current % i) ):
+		current /= i
+		high = i
+	
+	if( current == 1 ):
+		break
+
+	i += 2
+
+print "Largest prime factor of %d is %d\n" % (the_target, high)
+
