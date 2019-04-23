82e3b5ee49cbace515e757dc91a1e0d8cc27716f
kablondino <kablondino@gmail.com>
2019-04-23 16:50:32 +0200

    Full working Matlab/Octave problem

---
+++ Problem_3/problem_3.m
@@ -0,0 +1,29 @@
+%%
+% Problem 3
+% The prime factors of 13195 are 5, 7, 13 and 29.
+%
+% What is the largest prime factor of the number 600851475143 ?
+%%
+
+
+the_target = 600851475143;
+current = the_target;
+
+% Divisor
+i = 3;
+
+while true
+	while mod(current, i) == 0 
+		current = current / i;
+		high = i;
+	end
+	
+	if( current == 1 )
+		break
+	end
+
+	i = i + 2;
+end
+
+sprintf('Largest prime factor of %d is %d', the_target, high)
+
