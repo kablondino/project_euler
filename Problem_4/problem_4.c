4672e3fa44098b5125af45e87d09b9bdb1cd3f6e
kablondino <kablondino@gmail.com>
2019-04-24 02:41:40 +0200

    Fully FINALLY working programs

--- Problem_4/problem_4.c
+++ Problem_4/problem_4.c
@@ -4,3 +4,61 @@
  * Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int is_palindrome( unsigned long int a, unsigned long int b );
+char *strrev( char forward_string[] );
+
+
+int main() {
+	unsigned int max_palindrome = 0;  // Initialized final answer
+	int final_i = 0;
+	int final_j = 0;
+
+	for( int i = 999; i >= 100; i-- ) {
+		for( int j = i; j >= 100; j-- ) {
+			if( is_palindrome(i, j) == 1 && max_palindrome < i*j ) {
+					final_i = i; final_j = j;
+					max_palindrome = i*j;
+			}
+		}
+	}
+
+	printf( "%d * %d = %d\n", final_i, final_j, max_palindrome );
+
+	return 0;
+}
+
+
+int is_palindrome( unsigned long int a, unsigned long int b ) {
+	char possible_palindrome[12];  // Any 64-bit unsigned integer will fit this
+	// possible_palindrome becomes a string
+	sprintf( possible_palindrome, "%lu", a*b );
+	// Make a new string that is the reverse of possible_palindrome
+	char *possible_reversed = strrev(possible_palindrome);
+
+	// Declare integer versions of the strings above
+	int int_possible_palindrome = strtol( possible_palindrome, NULL, 10 );
+	int int_possible_reversed = strtol( possible_reversed, NULL, 10 );
+
+	if( int_possible_palindrome == int_possible_reversed ) { return 1; }
+	else { return 0; }
+}
+
+
+char *strrev( char forward_string[] ) {
+	// Reverses a string, and returns it as a char array
+	int length = strlen( forward_string );
+	// Initialize reversed string
+	char *reversed_string = (char *) malloc( sizeof(char) * length );
+
+	for( int k = 0; k <= length; k++ ) {
+		reversed_string[k] = forward_string[length - k - 1];
+	}
+
+	return reversed_string;
+}
+
--- Problem_4/problem_4.py
+++ Problem_4/problem_4.py
@@ -4,3 +4,30 @@
 
     Find the largest palindrome made from the product of two 3-digit numbers.
 """
+
+# Function to check if product is palindrome
+def is_palindrome(a, b):
+    # Convert the product to a string, and check if it is itself backwards
+    if str(a*b) == str(a*b)[::-1]:
+        return True
+    else:
+        return False
+
+# Store all palindromes of this problem in one dictionary, with the keys being
+# a tuple of the factors and the values being the palindrome
+list_palindrome = {}
+
+# Start from 999, and loop downwards to find palindrome
+for i in range( 999, 100, -1 ):
+    for j in range( i, 100, -1 ):
+        if is_palindrome(i, j) is True:
+            list_palindrome[(i,j)] = i*j  # Populate the dictionary
+
+# Set some convenience variables to aid in printing the answer
+first_factor = max(list_palindrome, key=list_palindrome.get)[0]
+second_factor = max(list_palindrome, key=list_palindrome.get)[1]
+
+largest_palindrome = list_palindrome[max(list_palindrome,
+    key=list_palindrome.get)]
+
+print(first_factor, "*", second_factor, "=", largest_palindrome)
