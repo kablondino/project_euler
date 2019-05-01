e1c8099e7e642432f35613d0b11f09d843dfd209
kablondino <kablondino@gmail.com>
2019-05-01 17:38:09 +0200

    Complete rewrite; working; needs investigating

--- Problem_4/problem_4.f95
+++ Problem_4/problem_4.f95
@@ -5,71 +5,54 @@
 ! Find the largest palindrome made from the product of two 3-digit numbers.
 !
 
-integer*8 function reverse_integer( forward_integer )
+function is_palindrome( x )
     implicit none
-    integer*8, intent(in) :: forward_integer
-    integer*8 :: reversed
-    integer*8 :: modify_integer
-    integer*8 :: remainder = 0
+    integer*8 :: x, i, j
+    logical :: is_palindrome
+    character(10) :: chars
 
-    modify_integer = forward_integer
-    print*, "2nd: modify_integer", modify_integer, "remainder: ", remainder, &
-        "reversed: ", reversed, "reverse_integer:", reverse_integer
+    write(chars, '(I10)') x
 
-    do while( modify_integer .ne. 0 )
-        remainder = mod(modify_integer, 10)
-        reversed = reversed*10 + remainder
-        modify_integer = modify_integer / 10
+    i = 1
+    j = 10
+
+    do while( chars(i:i) == ' ' )
+        i = i + 1
     enddo
 
-    print*, "2nd: modify_integer", modify_integer, "remainder: ", remainder, &
-        "reversed: ", reversed, "reverse_integer:", reverse_integer
+    do i = i, ((i + 10) / 2)
+        if( chars(i:i) .ne. chars(j:j) ) then
+            is_palindrome = .false.
+            return
+        endif
 
-    reverse_integer = reversed
+        j = j - 1
+    enddo
 
-end function reverse_integer
+    is_palindrome = .true.
+
+end function is_palindrome
 
 
 program problem_4
     implicit none
-    integer*8 :: reverse_integer  ! Initialize function call
-    integer*8 :: reversed
-    integer*8 :: max_palindrome = 0
-    integer*8 :: final_i = 0
-    integer*8 :: final_j = 0
+    logical :: is_palindrome
+    integer*8 :: i, j, final_i, final_j, max_palindrome
 
-    ! Loop counters
-    integer*8 :: i,j
-    ! Solution
-    integer*8 :: answer, reversed_answer
+    max_palindrome = 0
 
-!    i = 994
-!    j = 913
-!    answer = i*j
-!    reversed_answer = reverse_integer(i*j)
-!    print*, i, j, i*j, answer, reverse_integer(i*j), reverse_integer(answer), &
-!        reversed_answer
-!    if( i*j == reversed_answer ) then
-!        print*, "It's true!"
-!    else
-!        print*, "It's false..."
-!    endif
-
-    do i = 9, 1, -1
-        do j = i, 1, -1
-!            print*, i,j, i*j, reverse_integer(i*j), i*j == reverse_integer(i*j)
-            reversed_answer = reverse_integer(i*j)
-           ! print*, i,j, i*j, reversed_answer
-            if( i*j == reversed_answer .and. max_palindrome <= i*j ) then
+    do i = 999, 100, -1
+        do j = 999, 100, -1
+            if( i*j > max_palindrome .and. is_palindrome(i*j)) then
+                max_palindrome = i*j
                 final_i = i
                 final_j = j
-                max_palindrome = i*j
             endif
-            reversed_answer = 0
         enddo
     enddo
 
     write(*, '(I0, A, I0, A, I0)') final_i, " * ", final_j, " = ", &
         max_palindrome
+
 end program problem_4
 
