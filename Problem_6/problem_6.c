b65c22ba4bdc6fa5c883b0a6d4cab5382553ddd9
kablondino <kablondino@gmail.com>
2019-05-16 19:17:41 +0200

    Working solutions

--- Problem_6/problem_6.c
+++ Problem_6/problem_6.c
@@ -10,3 +10,23 @@
  * natural numbers and the square of the sum.
 */
 
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+	int sum_of_squares = 0;
+	int square_of_sum = 0;
+
+	for( int i = 1; i < 100; i++ ) {
+		sum_of_squares += pow(i, 2);
+		square_of_sum += i;
+	}
+
+	square_of_sum = pow(square_of_sum, 2);
+
+	printf("Sum of squares: %d\nSquare of sum: %d\nDifference: %d",
+			sum_of_squares, square_of_sum, square_of_sum - sum_of_squares);
+
+	return 0;
+}
--- Problem_6/problem_6.py
+++ Problem_6/problem_6.py
@@ -9,3 +9,15 @@
     Find the difference between the sum of the squares of the first one
     hundred natural numbers and the square of the sum.
 """
+
+# Initialize answers
+sum_of_squares, square_of_sum = 0, 0
+
+for i in range(1, 101):
+    sum_of_squares += i**2
+    square_of_sum += i
+
+square_of_sum = square_of_sum**2
+
+print(f"Sum of squares: {sum_of_squares}\n" f"Square of sum: {square_of_sum}\n"
+        f"Difference: {square_of_sum - sum_of_squares}")
