0c0cb576cac1c8c818ff93074eb683156f667ecc
kablondino <kablondino@gmail.com>
2019-06-08 17:20:45 +0200

    Fully-working (but slow)

--- Problem_7/problem_7.m
+++ Problem_7/problem_7.m
@@ -4,49 +4,52 @@
 %
 % What is the 10,001st (ten-thousand first) prime number?
 %%
-clc
-clear
+clc; clear; close all;
+
+
+counter = 1;
+target = uint64(3);
+
+while counter < 10001
+	if check_prime(target) == true
+		counter = counter + 1;
+	end
+	target = target + 2;
+end
+
+target = target - 2;  % Eliminate the last increment of 2 from the last loop
+
+answer = sprintf('The %d prime number is %d', counter, target);
+disp(answer)
+
 
 function is_prime = check_prime( number )
 	% Checks if the input number is prime
-	if number <= 1
+	if number <= 1 || ~isa(number, 'integer')
 		is_prime = false;
-		break;
+		return;
 	end
 
-	if number == 2
-		is_prime = true; break;
+	if number == uint64(2)
+		is_prime = true;
+		return;
 	else if mod(number, 2) == 0
-		is_prime = false; break;
+		is_prime = false;
+		return;
 	end
 
-	i = 3;
-	while i < int(number^0.5) + 1
+	i = uint64(3);
+	while i < uint64(floor(sqrt(double(number)))) + 1
 		if mod(number, i) == 0
-			is_prime = false; break;
+			is_prime = false;
+			return;
+			%break;
 		end
 		i = i + 1;
 	end
 
 	% If it went through all of the conditions, it must be prime
 	is_prime = true;
-	disp('HELLO! func')
+	return;
+    end
 end
-
-
-disp('HELLO! premain')
-counter = 1;
-target = 3;
-
-while counter < 10001
-	if is_prime(target) == true
-		counter = counter + 1;
-	end
-	target = target + 2;
-end
-
-target -= 2;  % Eliminate the last increment of 2 from the last loop
-
-disp('HELLO! postmain')
-sprintf('The %d prime number is %d', counter, target)
-
