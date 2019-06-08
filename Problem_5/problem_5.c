6bb4ca434d7ea9352cf46cc7d85a254473b489b5
kablondino <kablondino@gmail.com>
2019-06-08 23:12:33 +0200

    Heavy modification, but no solution yet

--- Problem_5/problem_5.c
+++ Problem_5/problem_5.c
@@ -7,38 +7,51 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef enum { false, true } bool;
 
-int *primes( int limit ) {
-	static int prime_list[100];
-	int array_position = 0;
-	bool else_loop = false;
+int Eratosthenes_Sieve( int *, int );
+int Sieve( int *, int );
 
-	for( int val = 1; val < limit + 1; val++ ) {
-		if( val > 1 ) {
-			for( int n = 2; n < val; n++ ) {
-				if( val % n == 0 ) {
-					else_loop = true;
-					break;
-				}
-			}
-			if( else_loop == true ) {
-				prime_list[array_position] = val;
-				array_position++;
+
+int main() {
+	int *array, n = 100;
+	array = (int *) malloc((n + 1) * sizeof(int));
+	printf("ANSWER! %d", Eratosthenes_Sieve(array, n));
+
+	return 0;
+}
+
+
+int Eratosthenes_Sieve( int *prime_list, int limit ) {
+    // Boolean list in which the position represents is_prime. Initialize to
+    // everything is True (a prime)
+	int prime_check[limit];
+	memset( prime_check, 0, limit*sizeof(int) );
+
+	for( int i = 0; i < limit; i++ ) {
+		prime_list[i] = 1;
+	}
+
+	int p = 2;
+	while( p*p <= limit ) {
+		if( prime_check[p] == 1 ) {
+			for( int j = p*p; j <= limit; j += p ) {
+				prime_check[j] = 0;
 			}
 		}
+		p++;
+	}
+
+	int k = 0;
+	for( p = 2; p <= limit; p++ ) {
+		if( prime_check[p] == 1 ) {
+			prime_list[k] = p;
+		}
+		k++;
 	}
 
 	return prime_list;
 }
 
-
-int main() {
-	int array_length = sizeof(primes(34)) / sizeof(primes(34)[0]);
-	for( int i = 0; i < array_length; i++ ) {
-		printf("%d %d\n", array_length, *primes( 34 ));
-	}
-
-	return 0;
-}
