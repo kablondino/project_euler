bb23a84295a3e246ae891beaf52f4d90681718a4
kablondino <kablondino@gmail.com>
2019-06-08 19:34:16 +0200

    Working, using function from problem_5.py

---
+++ Problem_10/problem_10.py
@@ -0,0 +1,11 @@
+"""
+    The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+    Find the sum of all the primes below two million.
+"""
+
+import sys
+sys.path.insert(0, '../Problem_5')
+from problem_5 import Eratosthenes_Sieve as primes_upto
+
+print(sum(primes_upto(int(2e6))))
