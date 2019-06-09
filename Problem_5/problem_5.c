d6850402b16a37597157016533d9c29620534914
kablondino <kablondino@gmail.com>
2019-06-09 22:16:08 +0200

    Sieve fully working; problem not yet solved

--- Problem_5/problem_5.c
+++ Problem_5/problem_5.c
@@ -8,50 +8,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+void Eratosthenes_Sieve( int, int[] );
 
 
-int Eratosthenes_Sieve( int *, int );
-int Sieve( int *, int );
+int main( int argc, char **argv ) {
+	char *ptr;  // Pointer for use in strtol
+	// Get the limit from the first command line argument
+	int limit = strtol( argv[1], &ptr, 10 );
+	// Initialize list to hold primes
+	int prime_list[limit];
 
+	Eratosthenes_Sieve( limit, prime_list );
 
-int main() {
-	int *array, n = 100;
-	array = (int *) malloc((n + 1) * sizeof(int));
-	printf("ANSWER! %d", Eratosthenes_Sieve(array, n));
+	for( int i = 0; i <= limit; i++ ) {
+		if( prime_list[i] == 1 )
+			printf("%d ", i);
+	}
 
 	return 0;
 }
 
 
-int Eratosthenes_Sieve( int *prime_list, int limit ) {
-    // Boolean list in which the position represents is_prime. Initialize to
-    // everything is True (a prime)
-	int prime_check[limit];
-	memset( prime_check, 0, limit*sizeof(int) );
+void Eratosthenes_Sieve( int limit, int prime_list[] ) {
+	// This function sets each element in `prime_list` to be either 0 or 1,
+	// composite or prime, which the index is the number is question.
 
-	for( int i = 0; i < limit; i++ ) {
+	// Set the first two entries (the numbers 0 and 1) as not prime
+	prime_list[0] = 0;
+	prime_list[1] = 0;
+	// Initialize the rest of the list to be prime
+	for( int i = 2; i < limit; i++ ) {
 		prime_list[i] = 1;
 	}
 
+	// Loop to mark off the values that are not prime, according to the method
 	int p = 2;
-	while( p*p <= limit ) {
-		if( prime_check[p] == 1 ) {
-			for( int j = p*p; j <= limit; j += p ) {
-				prime_check[j] = 0;
+	while( (p*p) <= limit ) {
+		if( prime_list[p] == 1 ) {
+			for( int i = (p*p); i <= limit; i += p ) {
+				prime_list[i] = 0;
 			}
 		}
 		p++;
 	}
-
-	int k = 0;
-	for( p = 2; p <= limit; p++ ) {
-		if( prime_check[p] == 1 ) {
-			prime_list[k] = p;
-		}
-		k++;
-	}
-
-	return prime_list;
 }
 
