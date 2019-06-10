89e61624ad5f5bf18923615d671019a53fe5ea17
kablondino <kablondino@gmail.com>
2019-06-10 22:42:32 +0200

    Fully working, using gcd and lcm

--- Problem_5/problem_5.go
+++ Problem_5/problem_5.go
@@ -5,3 +5,38 @@
  * What is the smallest positive number that is evenly divisible by all of the
  * numbers from 1 to 20?
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+
+func greatest_common_denominator(a, b uint64) uint64 {
+	for a != b {
+		if a > b {
+			a = a - b
+		} else {
+			b = b - a
+		}
+	}
+
+	return a
+}
+
+
+func least_common_multiple(a, b uint64) uint64 {
+	return ((a*b) / greatest_common_denominator(a, b))
+}
+
+
+func main() {
+	var answer uint64 = 1
+	for i := uint64(2); i <= 20; i++ {
+		answer = least_common_multiple(answer, i)
+	}
+
+	fmt.Println(answer)
+}
+
