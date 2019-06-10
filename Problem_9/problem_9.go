a80150bc0675230610df4a5c8e890854746733c8
kablondino <kablondino@gmail.com>
2019-06-11 00:48:02 +0200

    Pretty comment block; removed `math` import

--- Problem_9/problem_9.go
+++ Problem_9/problem_9.go
@@ -1,4 +1,4 @@
-/*
+/*****************************************************************************
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for
  * which,
  * 		a^2 + b^2 = c^2
@@ -6,21 +6,19 @@
  * 
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
-*/
+*****************************************************************************/
 
 package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	for a := 1; a <= 1000; a++ {
 		for b := 1; b <= 1000 - a; b++ {
 			c := 1000 - (a + b)
-			if( math.Pow(float64(a), 2) + math.Pow(float64(b), 2) ==
-				math.Pow(float64(c), 2) && a < b ) {
+			if( (a*a + b*b) == c*c && a < b ) {
 				fmt.Printf("%d*%d*%d = %d", a, b, c, a*b*c)
 			}
 		}
