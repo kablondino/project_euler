413ac453ef75bfe5696542bd33b874f266e00291
kablondino <kablondino@gmail.com>
2019-06-10 02:37:53 +0200

    Completely naïve way, copied from alternative.c

--- Problem_5/problem_5.f95
+++ Problem_5/problem_5.f95
@@ -4,3 +4,45 @@
 ! What is the smallest positive number that is evenly divisible by all of the
 ! numbers from 1 to 20?
 !
+
+FUNCTION greatest_common_denominator( m, n )
+    implicit none
+    integer*16 greatest_common_denominator
+    integer*16 m, n
+
+    do while(m .ne. n)
+        if( m > n ) then
+            m = m - n
+        else
+            n = n - m
+        endif
+    enddo
+
+END FUNCTION greatest_common_denominator
+
+
+FUNCTION least_common_multiple( a, b )
+    implicit none
+    integer*16 greatest_common_denominator, least_common_multiple
+    integer*16 a, b, this_gcd
+
+    this_gcd = greatest_common_denominator( a, b )
+    least_common_multiple = a*b / this_gcd
+
+END FUNCTION least_common_multiple
+
+
+PROGRAM problem_5
+    implicit none
+    integer*16 x, i
+    integer*16 greatest_common_denominator, least_common_multiple
+
+    do i = 2, 20
+        x = least_common_multiple(x, i)
+    enddo
+
+    print*, x
+
+END PROGRAM problem_5
+
+
