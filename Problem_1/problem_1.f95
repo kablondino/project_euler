a4effdb8f91c36d3780db365b13f64a4337eac5b
kablondino <kablondino@gmail.com>
2019-06-10 23:29:05 +0200

    Better comments, better var assignment

--- Problem_1/problem_1.f95
+++ Problem_1/problem_1.f95
@@ -1,15 +1,20 @@
+!-----------------------------------------------------------------------------
 ! Problem 1: Multiples of 3 and 5
 !
 ! If we list all the natural numbers below 10 that are multiples of 3
 ! or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 !
 ! Find the sum of all the multiples of 3 or 5 below 1000.
+!-----------------------------------------------------------------------------
 
 program problem1
     implicit none
-    integer :: i = 0
-    integer :: the_sum = 0
+    integer :: i, the_sum
 
+    the_sum = 0
+
+    ! Cycle through every number up to 1000, and check if it's a multiple
+    ! Note, it's "multiples of 3 or 5 BELOW 1000"
     do i = 0, 999
         if ( mod( i, 3 ) .eq. 0 .or. mod( i, 5 ) .eq. 0 ) THEN
             the_sum = the_sum + i
