016581fd697279581925edb7708d48843c3689cb
kablondino <kablondino@gmail.com>
2019-06-13 17:27:47 +0200

    Primes are now true; using p^2 algo

--- Problem_10/problem_10.f95
+++ Problem_10/problem_10.f95
@@ -7,32 +7,30 @@
 PROGRAM problem_10
     implicit none
     integer, parameter :: limit = 2000000
-    integer*16 :: i, j, k, the_sum
+    integer*16 :: i, j, p, the_sum
     logical, dimension(2:limit) :: the_sieve
 
-    ! Initialize the entire boolean sieve to false
-    the_sieve = .false.
+    ! Initialize the entire boolean sieve to true
+    ! `.false.` means composite (not prime)
+    the_sieve = .true.
 
-    ! Mark all of the even numbers past 2 as not prime
-    ! `.true.` means composite (not prime)
-    do i = 4, limit, 2
-        the_sieve(i) = .true.
-    enddo
-
-    ! Cross off the remaining odd composites
-    do i = 3, NINT(sqrt(real(limit))), 2
-        if( the_sieve(i) .eqv. .false. ) then
-            do j = i*i, limit, 2*i
-                the_sieve(j) = .true.
+    ! Incremental sieve in which the multiples of each prime `p` are generated
+    ! directly by counting up from the square of the prime in increments of `p`
+    p = 2
+    do while( p**2 <= limit )
+        if( the_sieve(p) .eqv. .true. ) then
+            do i = p**2, limit + 1, p
+                the_sieve(i) = .false.
             enddo
         endif
+        p = p + 1
     enddo
 
-    ! Sum the indices of the `the_sieve` array that are still .false.
+    ! Sum the indices of the `the_sieve` array that are still .true.
     the_sum = 0
-    do k = 2, limit
-        if( the_sieve(k) .eqv. .false. ) then
-            the_sum = the_sum + k
+    do j = 2, limit
+        if( the_sieve(j) .eqv. .true. ) then
+            the_sum = the_sum + j
         endif
     enddo
 
