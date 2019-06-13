9417268160682c9f7119ed6b4a88fdec72b809f1
kablondino <kablondino@gmail.com>
2019-06-13 11:20:08 +0200

    Function was too small, integrated into script

--- Problem_4/problem_4.m
+++ Problem_4/problem_4.m
@@ -10,8 +10,8 @@ clear; close all;
 max_palindrome = 0; final_i = 0; final_j = 0;
 
 for i = 999 : -1 : 100
-	for j = 999 : -1 : 100
-		if check_palindrome(i, j) && max_palindrome < i*j
+	for j = i : -1 : 100
+		if (num2str(i*j) == flip(num2str(i*j))) && max_palindrome < i*j
 			max_palindrome = i*j;
 			final_i = i; final_j = j;
 		end
