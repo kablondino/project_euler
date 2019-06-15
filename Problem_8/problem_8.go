66f5f8255a2d4f8e23ff12d83594e09b09be82ee
kablondino <kablondino@gmail.com>
2019-06-15 23:05:35 +0200

    Fully working program

--- Problem_8/problem_8.go
+++ Problem_8/problem_8.go
@@ -31,27 +31,78 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"regexp"
 	"strconv"
 )
 
-func main() {
 
-	input_large_number := "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
-
-	var largest_product int = 0
-	var adjacent_digits [13]int
-
-	for i := 0; i <= len(input_large_number) - len(adjacent_digits); i++ {
-		adjacent_digits, _ = strconv.Atoi(input_large_number[i:i+13])
-		fmt.Printf("%d", adjacent_digits)
-//		temp_product = math.Product(adjacent_digits)
-//
-//		if largest_product < temp_product {
-//			largest_product = temp_product
-//		}
+func get_digits() (digit_array []int64) {
+	// Read in number from file
+	content, err := ioutil.ReadFile("large_number.dat")
+	if err != nil {
+		fmt.Println("Could not open file for large number.")
 	}
 
-	fmt.Printf("%d", largest_product)
+	// Remove the \n (newline characters) so that it is one long string
+	// of numbers
+	re := regexp.MustCompile("\\n")
+	the_large_number := re.ReplaceAllString(string(content), "")
 
+	// Convert the long string of numbers into an array of integers
+	for _, character_value := range the_large_number {
+		i, err := strconv.ParseInt(string(character_value), 10, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		digit_array = append(digit_array, i)
+	}
+
+	return
+}
+
+
+func adjacent_digits_product( index, length int64, digit_array []int64 ) int64 {
+	// Calculate the product of the digits from a sub array of
+	// `digit_array`, with `length` as the length of the subarray
+	var product int64 = 1
+
+	for i := int64(0); i < length; i++ {
+		product *= digit_array[index + i]
+	}
+
+	return product
+}
+
+
+func main() {
+	// Length of subarray
+	const adjacent_length int64 = 13
+	// Initialize answer
+	var largest_product int64 = 0
+	// Array of digits that will be the constituent digits that multiply
+	// to the answer
+	var permanent_digits []int64
+
+	// Input the number as a large int64 array
+	digit_array := get_digits()
+
+	// Loop through
+	for j := int64(0); j < int64(len(digit_array)) - adjacent_length; j++ {
+		current_array := digit_array[j : j + adjacent_length]
+		product := adjacent_digits_product(j, adjacent_length, digit_array)
+		if product > largest_product {
+			largest_product = product
+			permanent_digits = current_array
+		}
+	}
+
+	// Format for ALL the printing
+	for k := int64(0); k < adjacent_length - 1; k++ {
+		fmt.Printf("%d*", permanent_digits[k])
+	}
+	fmt.Printf("%d = %d\n", permanent_digits[adjacent_length - 1],
+		largest_product)
 }
 
