e6e1d0da2dc763ab319bc7394bad8220c9596e04
kablondino <kablondino@gmail.com>
2019-06-17 03:20:13 +0200

    Slightly better docstrings

--- Problem_8/problem_8.f95
+++ Problem_8/problem_8.f95
@@ -1,4 +1,6 @@
 !-----------------------------------------------------------------------------
+! Problem 8: Largest product in a series
+!
 ! The four adjacent digits in the 1000-digit number that have the greatest
 ! product are 9 × 9 × 8 × 9 = 5832.
 !
--- Problem_9/problem_9.py
+++ Problem_9/problem_9.py
@@ -1,4 +1,6 @@
 """
+    Problem 9: Special Pythagorean triplet
+
     A Pythagorean triplet is a set of three natural numbers, a < b < c, for
     which,
                     a^2 + b^2 = c^2
