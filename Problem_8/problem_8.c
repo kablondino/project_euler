78868aa61cc329c10c3758fe916d8005b96107d9
kablondino <kablondino@gmail.com>
2019-06-17 02:06:18 +0200

    Fully working; will make func to read file

--- Problem_8/problem_8.c
+++ Problem_8/problem_8.c
@@ -28,38 +28,92 @@
 *****************************************************************************/
 
 #include <stdio.h>
+//#define uint unsigned long int
+
+long adjacent_digits_product( int, int, int[] );
 
 
-char *read_large_number( char filename, int length_of_file ) {
+int main() {
 
 	FILE *fp;
+	int length_of_file = 1020;  // Includes newline chars and \0
+	// Mark how many of the chars will be eliminated
+	int non_digit_chars = 20;
+	// Temporary character buffer for reading in all chars from file
 	char buff[length_of_file];
 
-	fp = fopen(filename, "r");
-	size_t value = fread( buff, 1, length_of_file, fp );
+	// Open the file and read it in to `buff` (including \n and \0)
+	fp = fopen("./large_number.dat", "r");
+	fread( buff, 1, length_of_file, fp );
+	// Initialize the digit array, with size ignoring \n and \0 chars
+	int input_large_number[length_of_file - non_digit_chars];
+	int buffer_index = 0;  // Index to track reading characters from file
 
-
-	char input_large_number[value];
-	size_t buffer_index = 0;
-
-	for( int i = 0; i < value; i++ ) {
+	// Populate the digit array, taking care of skipping the newline chars
+	for( int i = 0; i < length_of_file; i++ ) {
 		if( buff[i] != '\n' ) {
-			input_large_number[buffer_index] = buff[i];
+			// Set element, properly casting `char` to `int` with - '0'
+			input_large_number[buffer_index] = buff[i] - '0';
 			buffer_index++;
 		}
 		else { continue; }
 		if( buff[i] == '\0' ) { break; }
 	}
-
 	// Set the final character
 	input_large_number[buffer_index] = '\0';
 
-	return input_large_number;
-}
+	// Get size of current `input_large_number`
+	int len_input_large_number = sizeof(input_large_number) /
+		sizeof(input_large_number[0]);
 
-int main() {
 
-	read_large_number( "./large_number.dat", 1100 );
+	const int adjacent_length = 13;
+	// Array to hold final answer digits
+	int permanent_digits[adjacent_length];
+
+	// Temporary variable to store the calculated product
+	long the_product;
+	// Final answer, to be checked inside the loop
+	long largest_product;
+
+	// Loop through the chunk of digits, finding the largest product
+	for( int i = 0; i <= len_input_large_number - adjacent_length; i++ ) {
+		the_product = adjacent_digits_product( i, adjacent_length,
+				input_large_number );
+
+		// Check for largest product, and also set digits for printing
+		if( the_product > largest_product ) {
+			largest_product = the_product;
+			for( int j = 0; j < adjacent_length; j++ ) {
+				permanent_digits[j] = input_large_number[i + j];
+			}
+		}
+	}
+
+    // Format and do all the pretty printing
+	for( int k = 0; k < adjacent_length - 1; k++ ) {
+		printf("%d*", permanent_digits[k]);
+	}
+	printf("%d = %ld\n", permanent_digits[adjacent_length - 1],
+			largest_product);
+
 	return 0;
 }
 
+
+long adjacent_digits_product( int index, int length, int digit_array[] ) {
+	// Calculate the product of the digits from a sub array of
+	// `digit_array`, with `length` as the length of the subarray
+	long product = 1;
+
+	for( int i = 0; i < length; i++ ) {
+		if( digit_array[index + i] == 0 ) {
+			product = 0;
+			break;
+		}
+		else
+			product *= digit_array[index + i];
+	}
+
+	return product;
+}
