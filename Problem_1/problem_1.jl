d794377ec815279d389c766ff21257d69af47825
kablondino <kablondino@gmail.com>
2019-07-20 14:45:06 +0200

    Working julia solutions

---
+++ Problem_1/problem_1.jl
@@ -0,0 +1,21 @@
+#=
+    Problem 1: Multiples of 3 and 5
+
+    If we list all the natural numbers below 10 that are multiples of 3
+    or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+=#
+
+
+the_sum = 0
+
+# Cycle through every number up to 1000, and check if it's a multiple
+# Note, it's "multiples of 3 or 5 BELOW 1000"
+for i = 1:1000
+	if i % 3 == 0 || i % 5 == 0
+		global the_sum += i
+	end
+end
+
+println(the_sum)
