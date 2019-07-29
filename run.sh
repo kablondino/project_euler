321b3f75679b0b8b92961574575404f4395de94a
kablondino <kablondino@gmail.com>
2019-07-29 17:18:45 +0200

    `echo` to `printf`

--- run.sh
+++ run.sh
@@ -17,17 +17,16 @@ cyan=$(tput setaf 6)
 for i in $(seq 1 10)
 do
 	cd Problem_$i
-	echo ----------------------------------${bold}PROBLEM $i${normal}----------------------------------
+	printf -- "---------------------------------- ${bold}PROBLEM $i${normal} ----------------------------------\n"
 
 
 	# Run Python
 	if [ -f "problem_$i.py" ]
 	then
-		echo "${bold}${green}Python${normal}"
-		printf "\t${bold}"
+		printf "${bold}${green}Python${normal}\n\t${bold}"
 		\time -pf "${normal}%e s" python3 problem_$i.py
 	else
-		printf "${bold}${cyan}problem_$i.py file is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}problem_$i.py file is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -35,8 +34,7 @@ do
 	# Compile and run C
 	if [ -f "problem_$i.c" ]
 	then
-		echo "${bold}${blue}C${normal}"
-		printf "Compiling...\n"
+		printf "${bold}${blue}C${normal}\nCompiling...\n"
 		gcc -Wall problem_$i.c -o problem_$i\_c -lm
 		if [ $? = 0 ]
 		then  # If compile succeeded
@@ -44,10 +42,10 @@ do
 			\time -pf "${normal}%e s" ./problem_$i\_c
 			rm problem_$i\_c
 		else
-			printf "${red}${bold}Compiling failed.${normal} Skipping..."
+			printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
 		fi  # If compile failed
 	else
-		printf "${bold}${cyan}problem_$i.c file is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}problem_$i.c file is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -55,8 +53,7 @@ do
 	# Compile and run Fortran
 	if [ -f "problem_$i.f95" ]
 	then
-		echo "${bold}${blue}Fortran${normal}"
-		printf "Compiling...\n"
+		printf "${bold}${blue}Fortran${normal}\nCompiling...\n"
 		gfortran -Wall problem_$i.f95 -o problem_$i\_f
 		if [ $? -eq 0 ]
 		then  # If compile succeeded
@@ -64,10 +61,10 @@ do
 			\time -pf "${normal}%e s" ./problem_$i\_f
 			rm problem_$i\_f
 		else  # If compile failed
-			printf "${red}${bold}Compiling failed.${normal} Skipping..."
+			printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
 		fi
 	else
-		printf "${bold}${cyan}problem_$i.f file is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}problem_$i.f file is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -77,14 +74,13 @@ do
 	then
 		if [ -f "problem_$i.go" ]
 		then
-			echo "${bold}${blue}Go${normal}"
-			printf "\t${bold}"
+			printf "${bold}${blue}Go${normal}\n\t${bold}"
 			\time -pf "${normal}%e s" go run problem_$i.go
 		else
-			printf "${bold}${cyan}problem_$i.go file is not found.${normal} Skipping...\n"
+			printf "${bold}${cyan}problem_$i.go file is not found.${normal} Skipping...\n\n"
 		fi
 	else
-		printf "${bold}${cyan}Go compiler is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}Go compiler is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -94,14 +90,13 @@ do
 	then
 		if [ -f "problem_$i.m" ]
 		then
-			echo "${bold}${green}Matlab${normal}"
-			printf "\t"
+			printf "${bold}${green}Matlab${normal}\n\t"
 			matlab -nodesktop -nosplash -nojvm -r "run('problem_$i.m'); exit;" | tail -n +11
 		else
-			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n"
+			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
 		fi
 	else
-		printf "${bold}${cyan}Matlab is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}Matlab is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -111,14 +106,13 @@ do
 	then
 		if [ -f "problem_$i.m" ]
 		then
-			echo "${bold}${green}Octave${normal}"
-			printf "\t"
+			printf "${bold}${green}Octave${normal}\n\t"
 			octave -qf problem_$i.m
 		else
-			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n"
+			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
 		fi
 	else
-		printf "${bold}${cyan}Octave is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}Octave is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
@@ -128,14 +122,13 @@ do
 	then
 		if [ -f "problem_$i.jl" ]
 		then
-			echo "${bold}${green}Julia${normal}"
-			printf "\t${bold}"
+			printf "${bold}${green}Julia${normal}\n\t${bold}"
 			\time -pf "${normal}%e s" julia problem_$i.jl
 		else
-			printf "${bold}${cyan}problem_$i.jl file is not found.${normal} Skipping...\n"
+			printf "${bold}${cyan}problem_$i.jl file is not found.${normal} Skipping...\n\n"
 		fi
 	else
-		printf "${bold}${cyan}Julia is not found.${normal} Skipping...\n"
+		printf "${bold}${cyan}Julia is not found.${normal} Skipping...\n\n"
 	fi
 	printf "\n"
 
