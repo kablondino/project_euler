f15344ab84e0ca89550a6344a4ece367f5aea4a6
kablondino <kablondino>
2019-08-04 15:22:32 +0200

    Removed nonworking `omit` option; small refactor

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ Usage: $0 [options] [--]
 
 Arguments:
 
-  -h, --help
+  -h, --help, -?
     Display this usage information and exit.
 
   -s <val>, --start <val>, --start=<val>
@@ -18,10 +18,6 @@ Arguments:
 
   -e <val>, --end <val>, --end=<val>
     Problem number to end on. Defaults to 10.
-
-#  -o <val>, --omit <val>, --omit=<val>
-#    Run problems omitting the specified language. Indicate them with the
-#    lowercase file extensions, e.g. --omit=py for Python, -o f95 for Fortran.
 EOF
 }
 
@@ -40,8 +36,7 @@ while [ "$#" -gt 0 ]; do
 		--*'='*) shift; set -- "${arg%%=*}" "${arg#*=}" "$@"; continue;;
 		-s|--start) shift; start=$1;;
 		-e|--end) shift; end=$1;;
-		-o|--omit) shift; end=$1;;
-		-h|--help) usage; exit 0;;
+		-h|-\?|--help) usage; exit 0;;
 		--) shift; break;;
 		-*) usage_fatal "unknown option: '$1'";;
 		*) break;;  # Reached the end of the list
@@ -58,7 +53,6 @@ green=$(tput setaf 2)
 blue=$(tput setaf 4)
 pink=$(tput setaf 5)
 cyan=$(tput setaf 6)
-omit="octave"
 
 
 # Loop through each problem folder and run them
@@ -68,139 +62,119 @@ for i in $(seq $start $end); do
 
 
 	# Run Python
-	if [ "$omit" != "py" ]; then
-		if [ -f "problem_$i.py" ]; then
-			printf "${bold}${green}Python${normal}\n\t${bold}"
-			\time -pf "${normal}%e s" python3 problem_$i.py
-		else
-			printf "${bold}${cyan}problem_$i.py file is not found.${normal} Skipping...\n\n"
-		fi
-		printf "\n"
+	if [ -f "problem_$i.py" ]; then
+		printf "${bold}${green}Python${normal}\n\t${bold}"
+		\time -pf "${normal}%e s" python3 problem_$i.py
+	else
+		printf "${bold}${cyan}problem_$i.py file is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Compile and run C
-	if [ "$omit" != "c" ] || [ "$omit" != "C" ]; then
-		if [ -f "problem_$i.c" ]; then
-			printf "${bold}${blue}C${normal}\nCompiling...\n"
-			gcc -Wall problem_$i.c -o problem_$i\_c -lm
-			if [ $? = 0 ]
-			then  # If compile succeeded
-				printf "Compile complete. Running...\n\t${bold}"
-				\time -pf "${normal}%e s" ./problem_$i\_c
-				rm problem_$i\_c
-			else
-				printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
-			fi  # If compile failed
+	if [ -f "problem_$i.c" ]; then
+		printf "${bold}${blue}C${normal}\nCompiling...\n"
+		gcc -Wall problem_$i.c -o problem_$i\_c -lm
+		if [ $? = 0 ]
+		then  # If compile succeeded
+			printf "Compile complete. Running...\n\t${bold}"
+			\time -pf "${normal}%e s" ./problem_$i\_c
+			rm problem_$i\_c
 		else
-			printf "${bold}${cyan}problem_$i.c file is not found.${normal} Skipping...\n\n"
-		fi
-		printf "\n"
+			printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
+		fi  # If compile failed
+	else
+		printf "${bold}${cyan}problem_$i.c file is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Compile and run Fortran
-	if [ "$omit" != "f" ] || [ "$omit" != "f95" ]; then
-		if [ -f "problem_$i.f95" ]; then
-			printf "${bold}${blue}Fortran${normal}\nCompiling...\n"
-			gfortran -Wall problem_$i.f95 -o problem_$i\_f
-			if [ $? -eq 0 ]
-			then  # If compile succeeded
-				printf "Compile complete. Running...\n\t${bold}"
-				\time -pf "${normal}%e s" ./problem_$i\_f
-				rm problem_$i\_f
-			else  # If compile failed
-				printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
-			fi
-		else
-			printf "${bold}${cyan}problem_$i.f file is not found.${normal} Skipping...\n\n"
+	if [ -f "problem_$i.f95" ]; then
+		printf "${bold}${blue}Fortran${normal}\nCompiling...\n"
+		gfortran -Wall problem_$i.f95 -o problem_$i\_f
+		if [ $? -eq 0 ]
+		then  # If compile succeeded
+			printf "Compile complete. Running...\n\t${bold}"
+			\time -pf "${normal}%e s" ./problem_$i\_f
+			rm problem_$i\_f
+		else  # If compile failed
+			printf "${red}${bold}Compiling failed.${normal} Skipping...\n"
 		fi
-		printf "\n"
+	else
+		printf "${bold}${cyan}problem_$i.f file is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Run Go
-	if [ "$omit" != "go" ]; then
-		if [ -x "$(command -v go)" ]; then
-			if [ -f "problem_$i.go" ]
-			then
-				printf "${bold}${blue}Go${normal}\n\t${bold}"
-				\time -pf "${normal}%e s" go run problem_$i.go
-			else
-				printf "${bold}${cyan}problem_$i.go file is not found.${normal} Skipping...\n\n"
-			fi
+	if [ -x "$(command -v go)" ]; then
+		if [ -f "problem_$i.go" ]
+		then
+			printf "${bold}${blue}Go${normal}\n\t${bold}"
+			\time -pf "${normal}%e s" go run problem_$i.go
 		else
-			printf "${bold}${cyan}Go compiler is not found.${normal} Skipping...\n\n"
+			printf "${bold}${cyan}problem_$i.go file is not found.${normal} Skipping...\n\n"
 		fi
-		printf "\n"
+	else
+		printf "${bold}${cyan}Go compiler is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Run Matlab
-	if [ "$omit" != "matlab" ] || [ "$omit" != "m" ]; then
-		if [ -x "$(command -v matlab)" ]
+	if [ -x "$(command -v matlab)" ]; then
+		if [ -f "problem_$i.m" ]
 		then
-			if [ -f "problem_$i.m" ]
-			then
-				printf "${bold}${green}Matlab${normal}\n\t"
-				matlab -nodesktop -nosplash -nojvm -r "run('problem_$i.m'); exit;" | tail -n +11
-			else
-				printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
-			fi
+			printf "${bold}${green}Matlab${normal}\n\t"
+			matlab -nodesktop -nosplash -nojvm -r "run('problem_$i.m'); exit;" | tail -n +11
 		else
-			printf "${bold}${cyan}Matlab is not found.${normal} Skipping...\n\n"
+			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
 		fi
-		printf "\n"
+	else
+		printf "${bold}${cyan}Matlab is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Run Octave
-	if [ "$omit" != "octave" ] || [ "$omit" != "oct" ]; then
-		if [ -x "$(command -v octave)" ]
+	if [ -x "$(command -v octave)" ]; then
+		if [ -f "problem_$i.m" ]
 		then
-			if [ -f "problem_$i.m" ]
-			then
-				printf "${bold}${green}Octave${normal}\n\t"
-				octave -qf problem_$i.m
-			else
-				printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
-			fi
+			printf "${bold}${green}Octave${normal}\n\t"
+			octave -qf problem_$i.m
 		else
-			printf "${bold}${cyan}Octave is not found.${normal} Skipping...\n\n"
+			printf "${bold}${cyan}problem_$i.m file is not found.${normal} Skipping...\n\n"
 		fi
-		printf "\n"
+	else
+		printf "${bold}${cyan}Octave is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Run Julia
-	if [ "$omit" != "jl" ]; then
-		if [ -x "$(command -v julia)" ]
+	if [ -x "$(command -v julia)" ]; then
+		if [ -f "problem_$i.jl" ]
 		then
-			if [ -f "problem_$i.jl" ]
-			then
-				printf "${bold}${green}Julia${normal}\n\t${bold}"
-				\time -pf "${normal}%e s" julia problem_$i.jl
-			else
-				printf "${bold}${cyan}problem_$i.jl file is not found.${normal} Skipping...\n\n"
-			fi
+			printf "${bold}${green}Julia${normal}\n\t${bold}"
+			\time -pf "${normal}%e s" julia problem_$i.jl
 		else
-			printf "${bold}${cyan}Julia is not found.${normal} Skipping...\n\n"
+			printf "${bold}${cyan}problem_$i.jl file is not found.${normal} Skipping...\n\n"
 		fi
-		printf "\n"
+	else
+		printf "${bold}${cyan}Julia is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	# Run awk
-	if [ "$omit" != "awk" ]; then
-		if [ -f "problem_$i.awk" ]
-		then
-			printf "${bold}${green}Awk${normal}\n\t${bold}"
-			\time -pf "${normal}%e s" ./problem_$i.awk
-		else
-			printf "${bold}${cyan}problem_$i.awk file is not found.${normal} Skipping...\n\n"
-		fi
-		printf "\n"
+	if [ -f "problem_$i.awk" ]; then
+		printf "${bold}${green}Awk${normal}\n\t${bold}"
+		\time -pf "${normal}%e s" ./problem_$i.awk
+	else
+		printf "${bold}${cyan}problem_$i.awk file is not found.${normal} Skipping...\n\n"
 	fi
+	printf "\n"
 
 
 	cd ../
