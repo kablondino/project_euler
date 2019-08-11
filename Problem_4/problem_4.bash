654e30eac6d296671263a36010f6130a851d2c13
kablondino <kablondino>
2019-08-11 23:18:24 +0200

    Nonworking

--- Problem_4/problem_4.bash
+++ Problem_4/problem_4.bash
@@ -11,3 +11,30 @@
 # Function to check if product is palindrome
 
 
+function is_palindrome() {
+	local reversed_integer=$(echo $1 | rev)
+
+	if [ $reversed_integer -eq $1 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+max_palindrome=0
+final_i=0
+final_j=0
+
+for (( i=999; i>=100; i-- )); do
+	for (( j=i; j>=100; j-- )); do
+		if [ $(is_palindrome $(( i*j ))) -eq 0 ]; then
+			final_i=$i
+			final_j=$j
+			max_palindrome=$(( i*j ))
+		fi
+	done
+done
+
+printf "%d * %d = %d\n" $final_i $final_j $max_palindrome
+
