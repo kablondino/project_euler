8cdb24210e1847bfe5183563c4e20ab19c1ab2ce
kablondino <kablondino>
2019-08-13 22:06:52 +0200

    Preliminary, not to be used

--- Problem_5/problem_5.bash
+++ Problem_5/problem_5.bash
@@ -11,3 +11,36 @@
 #=============================================================================
 
 
+function Eratosthenes_Sieve() {
+	prime_list=()
+	prime_check=()
+	# Boolean list in which the position represents is_prime. Initialize to
+	# everything is True (a prime)
+	for i in $(seq 1 $1); do
+		prime_check+=(true)
+	done
+
+	# Incremental sieve in which the multiples of each prime `p` are generated
+	# by counting up from the square of the prime in increments of `p`
+	p=2
+	while [ $((p*p)) -le $1 ]; do
+		if ${prime_check[p]}; then
+			# Update all multiples of p up to the limit to not prime
+			for i in $(seq $((p*p)) $1 $p); do
+				prime_check[$i]=false
+			done
+		fi
+		p+=1
+	done
+
+	for p in $(seq 2 $1); do
+		if ${prime_check[$p]}; then
+			prime_list+=($p)
+		fi
+	done
+}
+
+Eratosthenes_Sieve 20
+declare -p prime_check
+declare -p prime_list
+
